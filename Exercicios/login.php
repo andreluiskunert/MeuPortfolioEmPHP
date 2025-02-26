cf56e75434fc808199272271710fa939e8584b3e
AnaliseeDesenvolvimentodeSistemas <you@example.com>
2025-02-25 22:42:25 -0300

    arquivos alterados

---
+++ Exercicios/login.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700" rel="stylesheet">
+    <link rel="stylesheet" href="recursos/css/estilo.css">
+    <link rel="stylesheet" href="recursos/css/login.css">
+    <title>Curso PHP</title>
+</head>
+<body class="login">
+    <header class="cabecalho">
+        <h1>Curso PHP</h1>
+        <h2>Seja Bem Vindo</h2>
+    </header>
+    <main class="principal">
+        <div class="conteudo">
+            <h3>Identifique-se</h3>
+                            <div class="erros">
+                                            <p>Usuário/Senha inválido!</p>
+                                    </div>
+                        <form action="#" method="post">
+                <div>
+                    <label for="email">E-mail</label>
+                    <input type="email" name="email" id="email">
+                </div>
+                <div>
+                    <label for="senha">Senha</label>
+                    <input type="password" name="senha" id="senha">
+                </div>
+                <button>Entrar</button>
+            </form>
+        </div>
+    </main>
+    <footer class="rodape">
+        COD3R & ALUNOS © 2025    </footer>
+</body>
+</html>
